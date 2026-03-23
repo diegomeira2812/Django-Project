185c504f6540a7a1bdbae12d355222b9f0b68367
Diego Jardim <diegomeira2812@gmail.com>
2026-03-23 12:24:56 -0300

    Adiciona edicao de tarefas (update)

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -30,3 +30,16 @@ def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.delete()
     return redirect("task_list")
+
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("task_list")
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, "tasks/update_task.html", {"form": form})
