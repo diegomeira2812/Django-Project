64c18aa16e1ec8b38dc91f737a18a8aad1e2094c
Diego Jardim <diegomeira2812@gmail.com>
2026-03-23 12:52:36 -0300

    Adiciona autenticacao de usuarios com login e logout

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
+@login_required
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, "tasks/task_list.html", {"tasks": tasks})
 
+@login_required
 def create_task(request):
     if request.method == "POST":
         form = TaskForm(request.POST)
@@ -20,17 +23,20 @@ def create_task(request):
 
     return render(request, "tasks/create_task.html", {"form": form})
 
+@login_required
 def complete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.completed =  True
     task.save()
     return redirect("task_list")
 
+@login_required
 def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.delete()
     return redirect("task_list")
 
+@login_required
 def update_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     
