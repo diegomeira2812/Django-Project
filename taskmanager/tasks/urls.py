d0276cab19b8c55f42b44fb724c803abd9f26334
Diego Jardim <diegomeira2812@gmail.com>
2026-03-25 10:46:09 -0300

    Adiciona endpoint API para listar tarefas em JSON

--- taskmanager/tasks/urls.py
+++ taskmanager/tasks/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("complete/<int:task_id>/", views.complete_task, name="complete_task"),
     path("delete/<int:task_id>/", views.delete_task, name="delete_task"),
     path("update/<int:task_id>/", views.update_task, name="update_task"),
+    path("api/tasks/", views.api_task_list),
 ]
--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 from django.contrib.auth.decorators import login_required
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+from .serializers import TaskSerializer
 
 
 # Create your views here.
+
+@api_view(['GET'])
+def api_task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
 
 @login_required
 def task_list(request):
