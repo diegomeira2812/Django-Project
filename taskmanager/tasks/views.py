af28f44b93ec4712aedfe9c5f38f6199be6bad7f
Diego Jardim <diegomeira2812@gmail.com>
2026-03-26 15:41:57 -0300

    Adiciona suporte a POST na API para criacao de tarefas

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -2,18 +2,29 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 from django.contrib.auth.decorators import login_required
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
+from rest_framework import status
 from .serializers import TaskSerializer
 
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def api_task_list(request):
-    tasks = Task.objects.filter(user=request.user)
-    serializer = TaskSerializer(tasks, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        tasks = Task.objects.filter(user=request.user)
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 @login_required
 def task_list(request):
